634d33e8f00c6c450e9eaf16ad61eb0d19975481
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-02-14 16:24:59 +0330

    add get and post functionality to post-controller

--- blog/src/main/java/com/spring/rest/controller/PostController.java
+++ blog/src/main/java/com/spring/rest/controller/PostController.java
@@ -1,10 +1,15 @@
 package com.spring.rest.controller;
 
+import com.spring.rest.exception.ResourceNotFoundException;
+import com.spring.rest.model.Post;
 import com.spring.rest.repository.CommentRepository;
 import com.spring.rest.repository.PostRepository;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -18,4 +23,37 @@ public class PostController {
         this.commentRepository = comment;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @GetMapping("")
+    public List<Post> listPosts() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Post getPost(@PathVariable(name = "id") Integer id) {
+        return postRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("no post found with id=" + id)
+        );
+    }
+
+    @PostMapping(value = "/{id}")
+    public Post getPost(@PathVariable(name = "id") Integer id, @RequestBody Post post) {
+        postRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException(("No post found with id=" + id))
+        );
+        return postRepository.save(post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable(name = "id") Integer id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException(("No post found with id=" + id))
+        );
+        postRepository.deleteById(post.getId());
+    }
 }
