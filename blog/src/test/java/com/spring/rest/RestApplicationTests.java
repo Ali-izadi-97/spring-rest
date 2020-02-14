99a5d11c5b767b41e1150bdbef52609be8338c4e
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-02-14 22:31:57 +0330

    add test method

--- blog/src/test/java/com/spring/rest/RestApplicationTests.java
+++ blog/src/test/java/com/spring/rest/RestApplicationTests.java
@@ -1,13 +1,59 @@
 package com.spring.rest;
 
+import com.spring.rest.model.Post;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class RestApplicationTests {
+    private static final String ROOT_URL = "http://localhost:8080";
+    RestTemplate restTemplate = new RestTemplate();
 
     @Test
-    void contextLoads() {
+    public void testGetAllPosts() {
+        ResponseEntity<Post[]> responseEntity = restTemplate.getForEntity(ROOT_URL + "/posts", Post[].class);
+        List<Post> posts = Arrays.asList(responseEntity.getBody());
+        assertNotNull(posts);
     }
 
+    @Test
+    public void testGetPostById() {
+        Post post = restTemplate.getForObject(ROOT_URL + "/posts/1", Post.class);
+        assertNotNull(post);
+    }
+
+    @Test
+    public void testCreatePost() {
+        Post post = new Post();
+        post.setTitle("Exploring SpringBoot REST");
+        post.setContent("SpringBoot is awesome!!");
+        post.setCreatedOn(new Date());
+        ResponseEntity<Post> postResponse =
+                restTemplate.postForEntity(ROOT_URL + "/posts", post, Post.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
+    }
+
+    @Test
+    public void testUpdatePost()
+    {
+        int id = 1;
+        Post oldPost = restTemplate.getForObject(ROOT_URL+"/posts/"+id, Post.class);
+        Post post = restTemplate.getForObject(ROOT_URL+"/posts/"+id, Post.class);
+        post.setContent("This my updated post1 content");
+        post.setUpdatedOn(new Date());
+        restTemplate.put(ROOT_URL+"/posts/"+id, post);
+        Post updatedPost = restTemplate.getForObject(ROOT_URL+"/posts/"+id, Post.class);
+        assertNotNull(updatedPost);
+        assertNotEquals(updatedPost, oldPost);
+    }
 }
